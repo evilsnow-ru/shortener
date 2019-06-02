6a6a51032f05de6142baca4bae982aaa25d8f472
Roman Samsonov <samsonov@bpcbt.com>
2019-06-03 01:35:01 +0300

    Lost test commit

--- main_test.go
+++ main_test.go
@@ -4,7 +4,21 @@ import "testing"
 
 func TestShortener(t *testing.T) {
 	shortener := New()
+	str1 := "some-long-text"
+	str2 := "another-long-text"
 
-	string := []string{"test-link", "test-link2"}
+	hash1 := shortener.Shorten(str1)
+
+	if hash1 == "" {
+		t.Fatal("Error generating short link for \"" + str1 + "\"")
+	}
+
+	hash2 := shortener.Shorten(str2)
+
+	if hash2 == "" {
+		t.Fatal("Error generating short link for \"" + str2 + "\"")
+	}
+
+
 
 }
